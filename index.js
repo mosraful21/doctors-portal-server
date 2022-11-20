dccf57a673804815c8e803bc5d69db577a5ea41a
mosraful21 <mosraful21@gmail.com>
2022-11-20 22:25:29 +0600

    doctors added

--- index.js
+++ index.js
@@ -24,7 +24,6 @@ function verifyJWT(req, res, next) {
     }
 
     const token = authHeader.split(' ')[1];
-
     jwt.verify(token, process.env.ACCESS_TOKEN, function (err, decoded) {
         if (err) {
             return res.status(403).send({ message: 'forbidden access' })
@@ -32,8 +31,6 @@ function verifyJWT(req, res, next) {
         req.decoded = decoded;
         next();
     })
-
-
 }
 
 async function run() {
@@ -41,6 +38,7 @@ async function run() {
         const appointmentOptionCollection = client.db('doctorsPortal').collection('appointmentOptions');
         const bookingsCollection = client.db('doctorsPortal').collection('bookings');
         const usersCollection = client.db('doctorsPortal').collection('users');
+        const doctorsCollection = client.db('doctorsPortal').collection('doctors');
 
         // Use Aggregate to query multiple collection and then merge data
         app.get('/appointmentOptions', async (req, res) => {
@@ -111,8 +109,8 @@ async function run() {
             const query = {}
             const result = await appointmentOptionCollection.find(query).project({name: 1}).toArray();
             res.send(result);
-        })
-        
+        });
+
         /***
          * API Naming Convention 
          * app.get('/bookings')
@@ -201,6 +199,18 @@ async function run() {
                 }
             }
             const result = await usersCollection.updateOne(filter, updateDoc, options);
+            res.send(result);
+        });
+
+        app.get('/doctors', async(req, res) => {
+            const query = {};
+            const doctors = await doctorsCollection.find(query).toArray();
+            res.send(doctors);
+        })
+
+        app.post('/doctors', async(req, res) => {
+            const doctor = req.body;
+            const result = await doctorsCollection.insertOne(doctor);
             res.send(result);
         });
 
